3efd73a210489b1c7fe06a87e6e549fd4a3aadb2
Yusha Syed <ysyed2@uwo.ca>
2025-09-08 04:06:56 -0400

    added letter count to stats.py

--- stats.py
+++ stats.py
@@ -4,8 +4,14 @@ def count_words(text: str):
     return count
 
 def num_words(text: str):
-    num = len(text)
-    return num
+    letters = {}
+    for letter in text:
+        letter = letter.lower()
+        if letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+    return letters
 
 if __name__ == "__main__":
     print(num_words("holy wow"))
