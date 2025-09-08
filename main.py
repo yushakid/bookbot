8de748ec2d238755ae06be13ad53927d7c59041c
Yusha Syed <ysyed2@uwo.ca>
2025-09-08 04:01:36 -0400

    added number of words function

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+from stats import count_words, num_words
+
 def get_book_text(filename: str):
     with open(filename, 'r', encoding = "UTF-8") as f:
         text = f.read()
     return text
 
-def count_words(text: str):
-    words = text.split(" ")
-    count = len(words)
-    return count
-
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(count_words(text))
+    print(num_words(text))
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,10 @@ def count_words(text: str):
     words = text.split(" ")
     count = len(words)
     return count
+
+def num_words(text: str):
+    num = len(text)
+    return num
+
+if __name__ == "__main__":
+    print(num_words("holy wow"))
